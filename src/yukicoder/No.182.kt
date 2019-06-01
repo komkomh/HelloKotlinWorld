e238dc0756e3e1f603d6dae4a205322867fa083a
komkomh <komkomh@gmail.com>
2019-06-01 11:59:12 +0900

    refactor No.182

--- src/yukicoder/No.182.kt
+++ src/yukicoder/No.182.kt
@@ -5,12 +5,8 @@ fun main() {
     val A: List<Int> = readLine()!!.split(" ").map(String::toInt)
 
     var aMap = mutableMapOf<Int, Int>()
-    for (a in A) {
-        aMap.get(a)?.also {
-            aMap.set(a, it + 1)
-        } ?: run {
-            aMap.set(a, 0)
-        }
+    A.forEach { a ->
+        aMap[a]?.also { aMap[a] = it + 1 } ?: run { aMap[a] = 0 }
     }
     println(aMap.count { it.value == 0 })
 }
